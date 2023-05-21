094130b8505b7c552c866de019814afb9c92772c
JULIAN BELDOTTI <121738722+JCBeldo@users.noreply.github.com>
2023-05-20 19:32:23 -0500

    Update show_spec.rb
    Remove commented out user stories

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,18 +1,5 @@
 require "rails_helper"
 
-#[ ] done
-
-# 1. Application Show Page
-
-# As a visitor
-# When I visit an applications show page
-# Then I can see the following:
-# - Name of the Applicant
-# - Full Address of the Applicant including street address, city, state, and zip code
-# - Description of why the applicant says they'd be a good home for this pet(s)
-# - names of all pets that this application is for (all names of pets should be links to their show page)
-# - The Application's status, either "In Progress", "Pending", "Accepted", or "Rejected"
-
 RSpec.describe "Applications Show Page", type: :feature do
 
   let!(:applicant1) { Application.create!( name: "Bruce", 
@@ -73,15 +60,3 @@ RSpec.describe "Applications Show Page", type: :feature do
     end
   end
 end
-
-# 4. Searching for Pets for an Application
-
-# As a visitor
-# When I visit an application's show page
-# And that application has not been submitted,
-# Then I see a section on the page to "Add a Pet to this Application"
-# In that section I see an input where I can search for Pets by name
-# When I fill in this field with a Pet's name
-# And I click submit,
-# Then I am taken back to the application show page
-# And under the search bar I see any Pet whose name matches my search
